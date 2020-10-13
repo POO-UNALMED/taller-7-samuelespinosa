5d6bd5d3ad9f50e0388045e2f5beb279730f9209
samuelespinosa <55595678+samuelespinosa@users.noreply.github.com>
2020-10-13 15:17:58 -0500

    assi error

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -1,11 +1,11 @@
 package comunicacion;
 
 public class Fabula extends Escrito {
-	private String enseñanza;
+	private String ensenanza;
 	private String interpretacion;
-	public Fabula(String origen, String titulo, String autor, int paginas,String enseñanza,String interpretacion) {
+	public Fabula(String origen, String titulo, String autor, int paginas,String ensenanza,String interpretacion) {
 		super(origen, titulo, autor, paginas);
-		this.enseñanza=enseñanza;
+		this.ensenanza=ensenanza;
 		this.interpretacion=interpretacion;
 	}
 
@@ -29,16 +29,16 @@ public class Fabula extends Escrito {
 				   this.getTitulo()+"\n"+
 				   this.getAutor()+"\n"+
 				   this.getPaginas()+"\n"+
-				   this.enseñanza;
+				   this.ensenanza;
 		return retorno;
 	}
 
-	public String getEnseñanza() {
-		return enseñanza;
+	public String getEnsenanza() {
+		return ensenanza;
 	}
 
-	public void setEnseñanza(String enseñanza) {
-		this.enseñanza = enseñanza;
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
 	}
 
 	public String getInterpretacion() {
